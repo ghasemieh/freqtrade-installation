817471af3a45ac6ea355e4fa7a6a03e74f017e97
Alireza Ghasemieh <11226765+ghasemieh@users.noreply.github.com>
2020-10-03 23:17:05 -0400

    Update setup_with_python.sh

--- setup_with_python.sh
+++ setup_with_python.sh
@@ -1,8 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
-sudo apt-get install python-pip python3-pip -y
+sudo apt install python3-pip -y
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev libhdf5-dev -y
-
 
 wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz
 tar xf Python-3.8.2.tgz
@@ -17,8 +16,6 @@ cd cmake-3.18.3
 ./bootstrap
 make
 sudo make install
-
-pip install wheel
 
 cd
 sudo apt-get install libffi-dev git libatlas-base-dev python3-venv cython -y
